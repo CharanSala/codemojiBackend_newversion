eb84181c07e057b90c7d0467870a4f1c38d0ed07
CharanSala <salacharan81@gmail.com>
2025-03-04 16:03:54 +0530

    Modified port config

--- Server.js
+++ Server.js
@@ -909,7 +909,7 @@ app.post('/compile', async (req, res) => {
 const port=process.env.PORT||5000;
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on ${port}`);
 });
 
 // Clean up temporary files on exit
