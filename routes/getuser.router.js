83d361765d8da7b3479be8ffb9220bd200eabefe
CharanSala <salacharan81@gmail.com>
2026-02-02 21:31:25 +0530

    correct the middleware spellinig

--- routes/getuser.router.js
+++ routes/getuser.router.js
@@ -3,5 +3,5 @@ import express from "express";
 import { authMiddleware } from "../middleware/auth.js";
 
 const router = express.Router();
-router.get("/getParticipant",  authMiddleware,getParticipant);
+router.get("/getParticipant", authMiddleware, getParticipant);
 export default router;
